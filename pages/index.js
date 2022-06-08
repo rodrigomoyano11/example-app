3e8de9e7e64d8533ff2302e1eca610ece50b265a
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2022-06-08 19:59:07 -0300

    Se aplicaron algunos estilos generales

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,68 @@
+/* eslint-disable @next/next/no-head-element */
+/* eslint-disable @next/next/no-page-custom-font */
 /* eslint-disable @next/next/no-html-link-for-pages */
 import React from 'react'
+import Button from '@mui/material/Button'
 
 const Home = () => (
   <div>
-    <header>
-      <h1>Inicio</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Inicio</a>
-          </li>
-          <li>
-            <a href="/about">Sobre</a>
-          </li>
-          <li>
-            <a href="/contact">Contato</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <head>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+    </head>
 
-    <main>
-      <section className="hero">
-        <h2>Esto es el Hero</h2>
-        <p>Esto es una descripción</p>
-        <button>Esto es un botón</button>
-      </section>
+    <body>
+      <header>
+        <h1>Inicio</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Inicio</a>
+            </li>
+            <li>
+              <a href="/about">Sobre</a>
+            </li>
+            <li>
+              <a href="/contact">Contato</a>
+            </li>
+          </ul>
+        </nav>
 
-      <section className="cards">
-        <h2>Cards</h2>
-        <div className="cards">
-          <div className="card">
-            <h3>Card 1</h3>
-            <p>Esto es una descripción</p>
-          </div>
-          <div className="card">
-            <h3>Card 2</h3>
-            <p>Esto es una descripción</p>
-          </div>
-          <div className="card">
-            <h3>Card 3</h3>
-            <p>Esto es una descripción</p>
-          </div>
-        </div>
-      </section>
-    </main>
+        <Button variant="contained">Esto es un botón</Button>
+      </header>
 
-    <footer>
-      <p>Esto es un footer</p>
-    </footer>
+      <main>
+        <section className="hero">
+          <h2>Esto es el Hero</h2>
+          <p>Esto es una descripción</p>
+          <Button variant="outlined">Esto es un botón</Button>
+        </section>
+
+        <section className="cards">
+          <h2>Cards</h2>
+          <div className="cards">
+            <div className="card">
+              <h3>Card 1</h3>
+              <p>Esto es una descripción</p>
+            </div>
+            <div className="card">
+              <h3>Card 2</h3>
+              <p>Esto es una descripción</p>
+            </div>
+            <div className="card">
+              <h3>Card 3</h3>
+              <p>Esto es una descripción</p>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <p>Esto es un footer</p>
+      </footer>
+    </body>
   </div>
 )
 
